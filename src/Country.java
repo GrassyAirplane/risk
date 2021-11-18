ff43f92a3b956a568d1eae7a9c7e14c4a05ce90f
Euan <62548894+GrassyAirplane@users.noreply.github.com>
2021-11-18 11:08:35 +0800

    Country Class Finished, Main Risk Test,

--- src/Country.java
+++ src/Country.java
@@ -1,5 +1,5 @@
-/* Developed By: ------
- * Revised Date: Nov 17, 2021 */
+/* Developed By: Euan Lim
+ * Revised Date: Nov 18, 2021 */
 
 /* Country Class */
 public class Country { 
@@ -7,7 +7,7 @@ public class Country {
    //instance variables
    private String countryName; 
    private int countryId; 
-   private int continendId;
+   private int continentId;
    private int[] adjacency;
    
    //additional variables
@@ -23,21 +23,65 @@ public class Country {
     *        adjacency    - id's of country current object is adjacent to */
    public Country( String countryName, int continentId, int[] adjacency ) {
       this.countryName = countryName;
-      this.continendId = continentId; 
+      this.continentId = continentId; 
       this.adjacency = adjacency;
+      //Sets country id to the current country count 
+      this.countryId = CountryCount; 
+      //Increments the county count
+      CountryCount ++; 
    }
    
    /* Gets the number of Troops in the country 
-    * @return - Troop Count*/
+    * @return - Troop Count */
    public int GetTroopCount() {
       return this.troopCount; 
    }
    
    /* Sets the number of Troops in the country 
-    * @param - New Troop Count*/
-   public void SetTroopCount(int troopCount) {
+    * @param - New Troop Count */
+   public void SetTroopCount( int troopCount ) {
       this.troopCount = troopCount; 
    }
    
-    
+   /* Gets the owner of the country
+    * @return - Owner object instance */
+   public Player GetOwner() {
+      return this.owner;
+   }
+   
+   /* Sets the owner of this country
+    * @param - owner object instance */
+   public void SetOwner( Player newOwner ) {
+      this.owner = newOwner;
+   }
+   
+   /* Gets the Country id
+    * @return - Country id int */
+   public int GetCountryId() {
+      return this.countryId; 
+   }
+   
+   /* Gets the Continent id 
+    * @return - Continent id int */
+   public int GetContinentId() {
+      return this.continentId; 
+   }
+   
+   /* Checks if inputted country is adjacent to this country 
+    * @param  - id of inputted country 
+    * @return - returns true or false depending on wether it is adjacent */
+   public boolean isAdjacent( int countryId ) {
+      //isAdjacent status
+      boolean isAdjacent = false; 
+      
+      //checks wether the inputted id is in the adjacency array
+      for( int index = 0; index < this.adjacency.length; index++ ) {
+         if( this.adjacency[index] == countryId ) {
+            isAdjacent = true; 
+         }
+      }
+      
+      //returns status of adjacency
+      return isAdjacent; 
+   }
 }
--- src/MainRisk.java
+++ src/MainRisk.java
@@ -6,5 +6,18 @@
 public class MainRisk {
    public static void main(String [] args) {
       //Main Code
+      
+      int[] blank = {0,5,3};
+      Country Malaysia = new Country("Malaysia", 1, blank);
+      Country Singapore = new Country("Singapore", 1, blank);
+      
+      System.out.println(Malaysia.GetCountryId() + " " + Malaysia.GetContinentId());
+      System.out.println(Singapore.GetCountryId() + " " + Singapore.GetContinentId());
+      
+      System.out.println(Malaysia.isAdjacent(Singapore.GetCountryId()));
+      
+      System.out.println(Singapore.isAdjacent(Malaysia.GetCountryId()));
+      //public Country( String countryName, int continentId, int[] adjacency ) {
+
    }
 }

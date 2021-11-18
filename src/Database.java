6fe1c8a6ed5ce4ddd38e1fce2de30a48e71f3bbe
Euan <62548894+GrassyAirplane@users.noreply.github.com>
2021-11-19 07:40:18 +0800

    Databse Class Revised

--- src/Database.java
+++ src/Database.java
@@ -1,18 +1,27 @@
 /* Developed By: Euan Lim
- * Revised Date: Nov 18, 2021 */
+ * Revised Date: Nov 19, 2021 */
 /* Import Statements*/
 import java.io.File;
-import java.util.Scanner; 
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /* Database */
 public class Database { 
    
    /* INSTANCE VARIABLE */
-   private Country[] allCountries;
+   private Country[] allCountries = new Country[42];
    private Card[] allCards;
+   
+   /* FILE VARIABLES */
+   private final static String FILE_PATH_COUNTRY = "../Countries.txt";
+   private final static String FILE_PATH_CARD = "";
    
    /*=============== CONSTRUCTOR ===============*/
    public Database() {
    }
    
+   private void importCountries(String fileName) throws FileNotFoundException {
+         //Create Scanner of file
+         Scanner scan = new Scanner(new File(fileName)); 
+   }
 }

6b020165b44cd01897d12180925d0eb810d25ca4
Euan <62548894+GrassyAirplane@users.noreply.github.com>
2021-11-19 20:20:14 +0800

    Bonus and Card

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,8 +1,24 @@
-/* Developed By: ------
- * Revised Date: Nov 17, 2021 */
+/* Developed By: Euan
+ * Revised Date: Nov 19, 2021 */
 
 /* Bonus Card Class
  * Parent  : Card */
 public class Bonus extends Card { 
+   /* INITIALIZED VARIABLES */
+   private int troopBonusType; 
    
+   /*=============== CONSTRUCTOR ===============*/
+   /* Constructor class for Card
+    * @param name             - Name of Card 
+    *         troopBonusType  - Type of troop */
+   public Bonus( String cardName, int troopBonusType ) {
+      super(cardName);
+      this.troopBonusType = troopBonusType; 
+   }
+   
+   /* Gets troopBonusType of card
+    * @return - troopBonusType*/
+   public int GetTroopBonusType() {
+      return this.troopBonusType;
+   }
 }
--- src/Card.java
+++ src/Card.java
@@ -4,6 +4,35 @@
 /* Card Class
  * Children  : Bonus
  *             Mission */
-public class Card { 
-
+public abstract class Card { 
+   /* INSTANCE VARIABLES */
+   private String cardName; 
+   private int cardId; 
+   
+   //keeps track of num Cards created
+   public static int CardCount = 0;
+   
+   /*=============== CONSTRUCTOR ===============*/
+   /* Constructor class for Card
+    * @param name - Name of Card */
+   public Card( String cardName ) {
+      this.cardName = cardName; 
+      this.cardId = CardCount; 
+      
+      //Increments Card Count
+      CardCount++;
+   }
+   
+   /*=============== PUBLIC METHODS ===============*/ 
+   /* Gets Card name
+    * @return     - returns the name of this card */
+   public String GetCardName() {
+      return this.cardName;
+   }
+   
+   /* Gets Card Id
+    * @return     - returns the id of this card */
+   public int GetCardId() {
+      return this.cardId; 
+   }
 }

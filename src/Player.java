43cac6ffd7e7959f8969ba06d51f7f821690539f
zoetay <zoetayss@gmail.com>
2021-11-19 15:25:40 +0800

    `

--- src/Player.java
+++ src/Player.java
@@ -40,6 +40,12 @@ public abstract class Player {
   
   /*=============== PUBLIC METHODS ===============*/
   
+  /* gets the icon used to represent the player.
+   * @return - char that represents player */
+  public char GetPlayerChar() {
+    return this.playerChar;
+  }
+  
   /* gets the number of troops.
    * @return - value of reinforcement variable. */
   public int GetReinforcement() {
@@ -136,8 +142,36 @@ public abstract class Player {
    * @param numDefender - number of troops player has to defend country
    * @return            - dice values in an array */
   public int[] Defender(int numDefender) {
-    int[] placeholder = {1};
-    return placeholder;
+    int[] die = {1, 2, 3, 4, 5, 6};
+    int[] rolls;
+    // rolling die
+    if (numDefender == 1) {
+      rolls = new int[1];
+      rolls[0] = new Random().nextInt(die.length);
+    }
+    else {
+      rolls = new int[2];
+      rolls[0] = new Random().nextInt(die.length);
+      rolls[1] = new Random().nextInt(die.length);
+    }
+    
+    // sorting the rolls array highest to lowest value
+    for (int i = 0; i < rolls.length - 1; i++) {
+      int currMax = rolls[i];
+      int currMaxIndex = i;
+      for (int j = i + 1; j < rolls.length; j++) {
+        if (rolls[j] > currMax) {
+          currMax = rolls[j];
+          currMaxIndex = j;
+        }
+      }
+      if (currMaxIndex != i) {
+        rolls[currMaxIndex] = rolls[i];
+        rolls[i] = currMax;
+      }
+    }
+    
+    return rolls;
   }
   
 }

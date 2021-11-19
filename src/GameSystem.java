4086d8fac9f4e7f1ee6fde6519baddd101ca96d9
zoetay <zoetayss@gmail.com>
2021-11-19 23:01:16 +0800

    Made Changes To Player

--- src/GameSystem.java
+++ src/GameSystem.java
@@ -4,4 +4,12 @@
 /* Attacker Player Class
  * Ability:  */
 public class GameSystem {
+  
+  /* GLOBAL CONSTANT VARIABLES */
+  // Troop Type
+  public final static int INFANTRY_TROOP = 1;
+  public final static int HORSE_TROOP    = 2;
+  public final static int CANNON_TROOP   = 3;
+  public final static int JOKER_TROOP    = 4;
+  
 }
---
+++ src/GameSystem.java~
@@ -0,0 +1,7 @@
+/* Developed By: ------
+ * Revised Date: Nov 17, 2021 */
+
+/* Attacker Player Class
+ * Ability:  */
+public class GameSystem {
+}
